d16c31a586703d8337495debda667c1849a6b4e7
Snapz01 <timmy.larsson@hotmail.com>
2024-05-10 01:29:10 +0200

    Add project files.

---
+++ Hangman/GenresCollection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.Emit;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+namespace Hangman
+{
+    public class GenresCollection
+    {
+        public static string[] animalWords = { "lion", "fish", "ferret", "goat", "crab", "wolverine", "cow", "horse", "crow", "meerkat", "seal", "llama", "leopard", "rhinoceros", "warthog", "ape", "chipmunk", "elephant", "deer", "walrus", "panda", "pig",
+    "parakeet", "beaver", "puma", "bison", "Eagle", "owl", "fox", "anteater", "groundhog", "lamb", "parrot", "panther", "coyote", "koala", "ram", "squirrel", "moose", "kitten", "otter", "octopus", "iguana", "sheep", "dog", "giraffe", "camel",
+    "kangaroo", "toad", "tiger", "sloth", "raccoon", "alligator", "weasel", "rooster", "jaguar", "snake", "wombat", "bear", "wolf", "chameleon", "hedgehog", "newt", "donkey", "salamander", "mole", "bat", "gorilla", "skunk", "rabbit",
+    "pony", "chimpanzee", "whale", "opossum", "monkey", "platypus", "buffalo", "zebra", "dolphin", "cat", "bull", "rat", "chinchilla", "reindeer", "armadillo", "antelope", "turtle", "puppy", "hippopotamus", "porcupine", "starfish", "hamster", "crocodile",
+    "chicken", "lemur", "ox", "bald", "eagle", "cheetah", "frog", "orangutan", "lizard", "yak", "mouse", "duck", "seahorse", "penguin", "ant", "spider", "ladybug", "butterfly", "worm", "dragonfly", "flamingo", "jellyfish", "moth", "ostrich",
+    "peacock", "shark"};
+
+
+    /*    public static string[] stringArrayFruits = {
+    "apple", "banana", "orange", "grape", "strawberry", "watermelon", "kiwi", "pineapple", "mango", "pear",
+    "peach", "plum", "cherry", "blueberry", "raspberry", "lemon", "lime", "avocado", "pomegranate", "coconut",
+    "apricot", "fig", "cranberry", "grapefruit", "papaya", "guava", "melon", "nectarine", "tangerine", "dragonfruit",
+    "lychee", "passionfruit", "persimmon", "starfruit", "blackberry", "boysenberry", "cantaloupe", "date", "elderberry",
+    "honeydew", "mulberry", "quince", "rhubarb", "soursop", "uglifruit", "ackee", "barberry", "cherimoya", "feijoa",
+    "gooseberry", "jabuticaba", "kumquat", "longan", "loquat", "maracuja", "rambutan", "salak", "tamarillo", "yuzu",
+    "breadfruit", "jackfruit", "durian", "custardapple", "plantain", "kiwano", "medlar", "miraclefruit", "pawpaw",
+    "physalis", "sapote", "tamarind", "ugni", "upupa", "yumberry", "zalzalak", "ziziphus", "jaboticaba", "guarana",
+    "hornedmelon", "mangosteen", "nance", "sugarapple", "jambul", "yangmei", "woodapple", "yewberry", "marula",
+    "saskatoonberry", "mirabelle", "bunyanut", "bilimbi", "fingerlime", "pindopalm", "gac", "bignay", "mamoncillo",
+    "feijoa", "kiwifruit", "loquat", "satsuma", "tamarillo", "uglifruit", "ugni", "watermelon"};
+        public static string FruitGenre()
+        {
+
+        }
+
+        public static string[] stringArrayColors = {
+    "red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "black", "white",
+    "gray", "turquoise", "maroon", "navy", "gold", "silver", "violet", "magenta", "cyan", "olive",
+    "teal", "indigo", "lavender", "peach", "beige", "coral", "salmon", "khaki", "lime",
+    "mintgreen", "ruby", "emerald", "sapphire", "topaz", "amber", "plum", "ivory", "charcoal", "taupe",
+    "rose", "copper", "bronze", "tan", "burgundy", "periwinkle", "chartreuse", "cerulean", "fuchsia",
+    "aqua", "orchid", "mauve", "aquamarine", "crimson", "pumpkin", "chocolate",
+    "mahogany", "russet", "azure", "saffron", "pearl", "sepia", "teal",
+    "limegreen", "olivegreen", "eggplant", "navyblue", "scarlet", "burntorange",
+    "mustardyellow", "seafoamgreen", "tealblue", "darkpurple", "brightpink", "steelblue", "sunfloweryellow",
+    "lavenderpurple", "peacockgreen", "brickred", "grassgreen", "hotpink", "babyblue", "bubblegumpink",
+    "darkred", "babypink", "paleblue", "deepblue", "brightorange", "royalblue", "lightpurple", "mintblue",
+    "goldenyellow", "rustorange", "lightgreen", "neongreen", "tangerineorange", "pastelpink", "pastelpurple",
+    "forestgreen", "neonpink", "royalpurple", "skyblue", "lightpink", "darkblue", "cherryred", "palepink"};
+        public static string ColorGenre()
+        {
+    */
+        }
+    }
---
+++ Hangman/program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hangman
+{
+    class Program
+    {
+        // lista för rätta bokstäver
+        static List<char> guessedLetters = new List<char>();
+
+        // listan för misslyckade bokstäver
+        static List<char> failedGuesses = new List<char>();
+
+        static void Main(string[] args)
+        {
+            ////// Meny loop //////
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+
+        static bool MainMenu()
+        {
+            ////// Menu ////////
+            Console.WriteLine(" O");
+            Console.WriteLine("/|\\");
+            Console.WriteLine("/\\");
+            Console.WriteLine("");
+            Console.WriteLine("Hangman Game");
+            Console.WriteLine("");
+            Console.WriteLine("Press 1 for animal genre");
+            //Console.WriteLine("Press 2 for fruit genre");
+            //Console.WriteLine("Press 3 for color genre");
+            Console.WriteLine("");
+            Console.WriteLine("Press 9 for exit");
+            Console.WriteLine("");
+            Console.Write("\r\nSelect an option: ");
+
+            // Keypress
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AnimalGenre();
+                    return true;
+                ///////////// fler genres ifall jag har tid /////////// nopes <<
+                //case "2":
+                //    fruitString();
+                //    return true;
+                //case "3":
+                //    colorString();
+                //    return true;
+                case "9":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        static void AnimalGenre()
+        {
+            //slumpar ett ord från GenresCollection  
+            //"$" för att gömma allt inom {}
+            string[] animalWords = GenresCollection.animalWords;
+            Random rand = new Random();
+            int animalIndex = rand.Next(0, animalWords.Length);
+            string randomAnimal = animalWords[animalIndex];
+            Console.WriteLine($"random animal name: {randomAnimal}");
+            Console.WriteLine($"Length of word: {randomAnimal.Length}");
+
+            Console.Clear();
+            Console.Write("You have chosen the 'animal' genre press anywhere to continue");
+            Console.ReadKey();
+            Console.Clear();
+
+            // gissningarna
+            int attempts = 0;
+            bool showMenu = true;
+            while (showMenu)
+            {
+                if (attempts >= 10)
+                {
+                    Console.WriteLine("You have exceeded the maximum number of attempts.");
+                    break;
+                }
+
+                showMenu = GuessMenu(randomAnimal);
+                attempts++;
+            }
+        }
+
+        static bool GuessMenu(string randomAnimal)
+        {
+            Console.WriteLine($"Attempts left: {10 - failedGuesses.Count}");
+
+            // visar bokstäver du gissade fel på
+            if (failedGuesses.Count > 0)
+            {
+                Console.WriteLine("Failed guesses:");
+                foreach (char failedGuess in failedGuesses)
+                {
+                    Console.Write(failedGuess + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Guess a letter or enter the whole word: ");
+            string guess = Console.ReadLine();
+
+            if (guess.Length == 1)
+            {
+                char letter = guess[0];
+                Console.WriteLine("You entered the letter: " + letter);
+
+                // spara gissade bokstäver
+                SaveGuessedLetter(letter, randomAnimal);
+
+                //gömmer ordet
+                string hiddenWord = HideWord(randomAnimal);
+                Console.WriteLine($"Hidden word: {hiddenWord}");
+
+                // kollar om ordet är gissat
+                if (hiddenWord == randomAnimal)
+                {
+                    Console.WriteLine("Congratulations! You guessed the word!");
+                    return false;
+                }
+            }
+            else
+            {
+                if (guess.ToLower() == randomAnimal.ToLower())
+                {
+                    Console.WriteLine("Congratulations! You guessed the word!");
+                    return false; 
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect guess!");
+                    failedGuesses.AddRange(guess.ToLower().Except(guessedLetters));
+                }
+            }
+
+            return true;
+        }
+
+        // spara gissade bokstäver
+        static void SaveGuessedLetter(char letter, string word)
+        {
+            guessedLetters.Add(letter);
+
+            // kollar om den gissade bokstaven inte är ordet
+            if (!word.Contains(letter))
+            {
+                failedGuesses.Add(letter);
+            }
+        }
+
+        //byter ut ordet till "_"
+        static string HideWord(string word)
+        {
+            string hiddenWord = "";
+            foreach (char letter in word)
+            {
+                if (char.IsLetter(letter))
+                {
+                    // Kollar om du gissat bokstaven
+                    if (guessedLetters.Contains(char.ToLower(letter)))
+                    {
+                        hiddenWord += letter;
+                    }
+                    else
+                    {
+                        hiddenWord += "_";
+                    }
+                }
+                else
+                {
+                    hiddenWord += letter;
+                }
+            }
+            return hiddenWord;
+        }
+    }
+}
